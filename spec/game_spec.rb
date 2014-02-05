b622fedad3cdb291c5dcefc5fa3887e4e290d57d
LNA <ThisIsLaToyaNicole@gmail.com>
2014-02-05 12:23:16 -0600

    cleaned up #ai_game_loop spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,14 +9,6 @@ describe Game do
   let (:mock_ui) {MockUI.new}
   let (:mock_game_state) {MockGameState.new}
   let (:all_human_game) {game_options.update(:player_one => 'human', :player_two => 'human')}
-  
-  # before(:each) do
-  #   @game = Game.new
-  #   @mock_ui = MockUI.new
-  #   @mock_game_state = MockGameState.new
-  #   @game.ui = @mock_ui
-  #   @game.game_state = @mock_game_state
-  # end
 
   context '#set_player_types' do 
     it "updates player_one and player_two from game_options" do 
@@ -39,53 +31,66 @@ describe Game do
     end
 
     it "gets a move from the ui" do
-      @game.human_game_loop
+      game.game_state = mock_game_state
+      game.ui = mock_ui
 
-      @mock_ui.provided_move.should == true
+      game.human_game_loop
+
+      mock_ui.provided_move.should == true
     end
 
     it "checks game state to see if move is valid" do
-      @mock_ui.stored_moves = [0]
-      
-      @game.human_game_loop
-      @mock_game_state.checked_move.should == true
+      mock_ui.stored_moves = [0]
+      game.game_state = mock_game_state
+      game.ui = mock_ui
+      game.human_game_loop
+
+      game.game_state.checked_move.should == true
     end
     
     it "finds the proper game state based of the players move" do
-      @game.human_game_loop
+      game.game_state = mock_game_state
+      game.ui = mock_ui
+      game.human_game_loop
 
-      @mock_game_state.found_game_state.should == true
+      mock_game_state.found_game_state.should == true
     end
 
     it "should trigger invalid move message if invalid" do
-      @mock_ui.stored_moves = ["0", "1"]
+      mock_ui.stored_moves = ["0", "1"]
       test_game_state = GameState.new('X', ['O', nil])
-      @game.game_state = test_game_state
-      @game.human_game_loop
+      game.game_state = test_game_state
+      game.human_game_loop
 
-      @mock_ui.invalid_message_sent.should == true
+      mock_ui.invalid_message_sent.should == true
     end
   end
 
   context "#winner" do
     it "asks the game_state for the final state" do
-      @game.winner
+      game.game_state = mock_game_state
+      game.ui = mock_ui
+      game.winner
 
-      @mock_game_state.checked_final_state.should == true
+      mock_game_state.checked_final_state.should == true
     end
   end
 
   context "#ai_game_loop" do
     it "plays the best ai move" do
-      @game.ai_game_loop
+      game.game_state = mock_game_state
+      game.ui = mock_ui
+      game.ai_game_loop
 
-      @mock_game_state.played_move.should == true
+      mock_game_state.played_move.should == true
     end
 
     it "displays grid after AI move" do
-      @game.ai_game_loop
+      game.game_state = mock_game_state
+      game.ui = mock_ui
+      game.ai_game_loop
 
-      @mock_ui.displayed_updated_board.should == true
+      mock_ui.displayed_updated_board.should == true
     end
   end 
 end  
