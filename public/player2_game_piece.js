0e0c8305448bd5c5902b94fcf45936e2f1bd988b
LNA <ThisIsLaToyaNicole@gmail.com>
2014-02-06 14:44:21 -0600

    refactored method to

--- public/player2_game_piece.js
+++ public/player2_game_piece.js
@@ -1,23 +1,6 @@
 $(function() {
   $('#player1_game_piece').on ('change', function(event){
-    var $this = $(this);
-    if (($this).val() == "X") {
-      $('#player2_game_piece').val("O");
-    }
-    else{
-      $('#player2_game_piece').val("X");
-    }
-  });
-});
-
-$(function() {
-  $('#player2_game_piece').on ('change', function(event){
-    var $this = $(this);
-    if (($this).val() == "X") {
-      $('#player1_game_piece').val("O");
-    }
-    else{
-      $('#player1_game_piece').val("X");
-    }
+      if ($(this).val() == 'X') $('#player2_game_piece').val("O");
+        else $('#player2_game_piece').val("X");   // will select the non-selected option
   });
 });
