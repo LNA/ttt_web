aaca8d645aa43e82ff2e785b2668fac34af41d21
LNA <LAllen@8thLight.com>
2014-04-07 16:47:58 -0500

    refactors method that returns @space

--- lib/ai.rb
+++ lib/ai.rb
@@ -1,6 +1,6 @@
 require 'game_rules'
 class AI
-  attr_accessor :game_piece, :game_over
+  attr_accessor :game_piece, :game_over, :space
   def initialize(game_rules)
     @game_rules = game_rules 
   end
@@ -10,13 +10,18 @@ class AI
       if player == nil 
         next_board = spaces.dup
         next_board[space] = @game_piece
-        if @game_rules.game_over?(next_board)
-          if rank(next_board) == 1
-            return space
-          end
-        end
+        return_space_to_be_played(next_board, space)
       end 
     end 
+    @space
+  end
+
+  def return_space_to_be_played(next_board, space)
+    if @game_rules.game_over?(next_board)
+      if rank(next_board) == 1
+        @space = space
+      end
+    end
   end
 
   def rank(spaces)
@@ -24,8 +29,6 @@ class AI
   end
 
   def intermediate_state_rank(spaces)
-    require 'pry'
-        binding.pry
     ranks = spaces.collect{ |game_state| game_state.rank }
     if @game.current_player == game_piece
       ranks.max
