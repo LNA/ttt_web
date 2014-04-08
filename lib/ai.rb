8a06889cacdf4ad9176af6ceaba692c3b0fa9b99
LNA <LAllen@8thLight.com>
2014-04-08 10:36:34 -0500

    Plays a winning move instead of blocking an opponent

--- lib/ai.rb
+++ lib/ai.rb
@@ -1,47 +1,58 @@
 require 'game_rules'
+require 'board'
+
 class AI
   attr_accessor :game_piece, :game_over, :space
-  def initialize(game_rules)
+  def initialize(game_rules, board, max_player)
     @game_rules = game_rules 
+    @board = board
+    @max_player = max_player
   end
 
   def find_best_move(spaces, depth=0)
+    @open_spaces = []
     spaces.each_with_index do |player, space|
       if player == nil 
+        @open_spaces << space
         next_board = spaces.dup
         next_board[space] = @game_piece
-        return_space_to_be_played(next_board, space)
+        return_space_to_be_played(next_board, space, spaces)
       end 
     end 
     @space
   end
+  
+private
 
-  def return_space_to_be_played(next_board, space)
+  def return_space_to_be_played(next_board, space, spaces)
     if @game_rules.game_over?(next_board)
-      if rank(next_board) == 1
+      if rank(next_board, @max_player) == 1
+        @space = space
+      end
+    else
+      block_max_player_win(spaces, @max_player)
+    end
+  end
+
+  def block_max_player_win(spaces, max_player)
+    @open_spaces.each do |space|
+      current_spaces = spaces.dup
+      current_spaces[space] = @max_player
+      if final_state_rank(current_spaces, @max_player) == -1
         @space = space
       end
     end
   end
 
-  def rank(spaces)
-    @rank ||= final_state_rank(spaces) || intermediate_state_rank(spaces)
+  def rank(spaces, max_player)
+    @rank ||= final_state_rank(spaces, @max_player) 
   end
 
-  def intermediate_state_rank(spaces)
-    ranks = spaces.collect{ |game_state| game_state.rank }
-    if @game.current_player == game_piece
-      ranks.max
-    else
-      ranks.min
-    end
-  end 
-
-
-  def final_state_rank(spaces)
+  def final_state_rank(spaces, max_player)
     if @game_rules.game_over?(spaces).class == String
       return 0 if @game_rules.tie?(spaces)
-      @game_rules.winner?(spaces) == @game_piece ? 1 : -1
+      return 1  if @game_rules.winner?(spaces) == @game_piece
+      return  -1 if @game_rules.winner?(spaces) == @max_player
     end
   end
 end
--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -1,19 +1,48 @@
 require 'ai'
-require 'board'
 require 'game_rules'
 
 describe AI do 
-
-  let (:game_rules) {GameRules.new}
-  let (:ai)         {AI.new(game_rules)}
   let (:board)      {Board.new}
+  let (:game_rules) {GameRules.new}
 
-  it "finds the best move for the first level" do 
-    ai.game_piece = "O"
-    board.spaces = [nil, "X", "O", 
-                    "X", nil, "X", 
-                    nil, "O", "O"]
+  before :each do
+    @ai = AI.new(game_rules, board, "X")
+    @ai.game_piece = "O"
+  end
 
-    ai.find_best_move(board.spaces).should == 6
+  context 'blocking opponent wins' do
+    it "finds the best move for a game with a depth of zero" do 
+      spaces = [nil, "X", "O", 
+                "O", nil, nil, 
+                nil, "X", "X"]
+
+      @ai.find_best_move(spaces).should == 6
+    end
+
+    it "blocks an opponent win" do 
+      spaces = [nil, nil, "X", 
+                "X", nil, "X", 
+                nil, nil, "O"]
+
+      @ai.find_best_move(spaces).should == 4
+    end
+  end
+
+  context 'playing a winning move' do
+    it "plays a winning move instead" do
+      spaces = [nil, nil, "X", 
+                nil, nil, "X", 
+                "O", nil, "O"]
+
+      @ai.find_best_move(spaces).should == 7
+    end
+
+    it "plays a winning move instead of blocking an opponent" do 
+      spaces = [nil, nil, "X", 
+                "X", nil, "X", 
+                "O", nil, "O"]
+
+      @ai.find_best_move(spaces).should == 7
+    end
   end
 end
