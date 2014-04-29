0aed0e8fe2c8d6df9ec2acb1babd73ebd3572a22
LNA <LAllen@8thLight.com>
2014-04-29 18:10:25 -0500

    Pulls a Malcolm

--- lib/ai.rb
+++ lib/ai.rb
@@ -7,43 +7,15 @@ class AI
     @game_rules = game_rules
   end
 
- WIN, TIE, LOSS = 100, 0, -100
+  WIN, TIE, LOSS = 100, 0, -100
  
    def find_best_move(board)
-    best_score, @possible_moves = TIE, {}
-    current_player = 'X'
-  
-    board.open_spaces.each do |move|
-      depth = 1
-      make_move(board, move, next_player(current_player))
-      score = rank(board.spaces) - depth 
-      track_possible_moves(board, depth, move)
-      return move if score == 99 
-      score_available_moves(board, depth, next_player(current_player), move)
-      board = reset(board, move)
-    end
-    best_score = @possible_moves.values.max
-    best_move = @possible_moves.key(best_score)
   end
 
   def score_available_moves(board, depth, current_player, move)
-    if @game_rules.game_over?(board.spaces) 
-       track_possible_moves(board, depth, move)
-    else
-      depth += 1
-      board.open_spaces.each do |move|
-        cloned_board = board.clone
-        make_move(cloned_board, move, current_player)
-        score_available_moves(cloned_board, depth, next_player(current_player), move)
-        board = reset(cloned_board, move)
-      end
-    end
   end
 
   def track_possible_moves(board, depth, move)
-    score = (rank(board.spaces)).abs - depth if @game_rules.winner(board.spaces) == 'X'
-    score = rank(board.spaces) - depth if @game_rules.winner(board.spaces) == 'O'
-    score = rank(board.spaces) if @game_rules.winner(board.spaces) == false 
     @possible_moves[move] = score #only replace if its another score
   end
 
