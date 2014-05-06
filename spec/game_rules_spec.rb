599b35f5a63c5502af2b9305fd566a76f90917aa
LNA <LAllen@8thLight.com>
2014-05-06 14:18:49 -0500

    Cleans up spec for rules of the game

--- spec/game_rules_spec.rb
+++ spec/game_rules_spec.rb
@@ -21,9 +21,10 @@ describe GameRules do
     end
   end
 
-  context "#full_board?" do 
+  context "#full?" do 
     it "returns false for a board that is not full" do
       board.spaces = [nil] * 9
+
       game_rules.full?(board.spaces).should == false
     end
 
@@ -34,19 +35,39 @@ describe GameRules do
     end
   end 
 
+  context "#tie?" do 
+    it 'returns true in the case of a tie' do 
+      board.spaces[0] = 'X'
+      board.spaces[1] = 'X'
+      board.spaces[2] = 'O'
+      board.spaces[3] = 'O'
+      board.spaces[4] = 'O'
+      board.spaces[5] = 'X'
+      board.spaces[6] = 'X'
+      board.spaces[7] = 'X'
+      board.spaces[8] = 'O'
+
+      game_rules.tie?(board.spaces).should == true
+    end
+
+    it 'returns false in the case of a win' do 
+      board.spaces = ["X"] * 9
+
+      game_rules.tie?(board.spaces).should == false
+    end
+
+    it 'returns false in the case of an empty board' do
+      board.spaces = [nil] * 9
+
+      game_rules.tie?(board.spaces).should == false
+    end
+  end
+
   context "#game_over?" do 
     it "returns true for a full board" do 
       board.spaces = ["X"]*9
 
       game_rules.game_over?(board.spaces).should == true
-    end
-
-    it "returns false in the case of a tie" do 
-      board.spaces = ["X", "X", "O", 
-                      "O", "O", "X", 
-                      "X", "X", "O"]
-
-      game_rules.winner(board.spaces).should == false
     end
 
     it "returns false if the game is not over" do       
@@ -56,13 +77,27 @@ describe GameRules do
 
   context "#winner" do 
     it "returns the winner" do 
-      board.spaces = ['X']*9
+      board.spaces = ['X'] * 9
 
       game_rules.winner(board.spaces).should == "X"
     end
 
     it "returns the false if there is no winner" do 
-      board.spaces = [nil]*9
+      board.spaces = [nil] * 9
+
+      game_rules.winner(board.spaces).should == false
+    end
+
+    it "returns false in the case of a tie" do 
+      board.spaces[0] = 'X'
+      board.spaces[1] = 'X'
+      board.spaces[2] = 'O'
+      board.spaces[3] = 'O'
+      board.spaces[4] = 'O'
+      board.spaces[5] = 'X'
+      board.spaces[6] = 'X'
+      board.spaces[7] = 'X'
+      board.spaces[8] = 'O'
 
       game_rules.winner(board.spaces).should == false
     end
