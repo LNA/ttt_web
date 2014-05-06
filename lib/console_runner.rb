aaca8e6c372adde2f9cdc1ecbaf4d430d05f9a73
LNA <LAllen@8thLight.com>
2014-05-06 13:29:12 -0500

    Renames method that makes a move

--- lib/console_runner.rb
+++ lib/console_runner.rb
@@ -28,14 +28,14 @@ private
 
   def play_game(player_one_type, player_two_type, player_one_game_piece, player_two_game_piece)
     until @game_rules.game_over?(@board.spaces) 
-      turn(player_one_type, player_one_game_piece, player_two_game_piece)
+      make_move(player_one_type, player_one_game_piece, player_two_game_piece)
       @ui.display_grid(@board.spaces)
-      turn(player_two_type, player_two_game_piece, player_one_game_piece)
+      make_move(player_two_type, player_two_game_piece, player_one_game_piece)
       @ui.display_grid(@board.spaces)
     end
   end
 
-  def turn(current_player_type, game_piece, opponent_game_piece)
+  def make_move(current_player_type, game_piece, opponent_game_piece)
     make_human_move(current_player_type, game_piece, opponent_game_piece) if current_player_type == "H" 
     make_ai_move(game_piece, opponent_game_piece) if current_player_type == "A"
     check_for_winner
@@ -52,7 +52,7 @@ private
   def check_validity_of_move(current_player_type, game_piece, opponent_game_piece)
     if @game_rules.valid?(@move, @board) == false
       @ui.invalid_move_message
-      turn(current_player_type, game_piece, opponent_game_piece)
+      make_move(current_player_type, game_piece, opponent_game_piece)
     else
       @board.fill(@move, game_piece)
     end
